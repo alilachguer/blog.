4e82a3ca9aaf9c7fd40a5dc1ac090f71a4333157
alilachguer <alilachguer95@gmail.com>
2019-01-02 11:37:04 +0100

    couple of quick changes

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -5,17 +5,21 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use AppBundle\Entity\Blog;
+use Symfony\Component\HttpFoundation\Request;
 
 class BlogController extends Controller
 {
     /**
      * @Route("/", name="blog")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $repository = $this->getDoctrine()->getRepository(Blog\Post::class);
         $blogs = $repository->findBy([], ['publishedDatetime' => 'DESC'], 10, 0);
         // findBy([], ['published'] => 'DESC'], 10, 0);
+
+        $paginator  = $this->get('knp_paginator');
+        $blogs = $paginator->paginate($blogs, $request->query->get('page', 1), 5);
 
         return $this->render('index.html.twig', array(
             'blogs' => $blogs
